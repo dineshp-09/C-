e3bbd4a6325c9e5edecf0fcd88ff399f5e14e8aa
Dinesh Patil <44475250+dineshp-09@users.noreply.github.com>
2019-11-05 00:10:34 +0800

    Create singly_linked_list.cpp

---
+++ singly_linked_list.cpp
@@ -0,0 +1,96 @@
+/*
+Singly Linked List Implementation 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+};
+
+class LinkedList {
+    Node *head;
+    public:
+        LinkedList() {
+            head = NULL;
+        }
+        void addFirst(int);
+        void addLast(int);
+        void insertPosition(int, int);
+        // void deletePosition(int, int);
+        void display();
+};
+
+void LinkedList::addFirst(int val) {
+    Node *newNode = new Node();
+    newNode->data = val;
+    newNode->next = NULL;
+    if (head == NULL) {
+        head = newNode;
+        newNode = NULL;
+    } else {
+        newNode->next = head;
+        head = newNode;
+    }
+}
+
+void LinkedList::addLast(int val) {
+    Node *newNode = new Node();
+    newNode->data = val;
+    newNode->next = NULL;
+    Node * temp = head;
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void LinkedList::insertPosition(int pos, int val) {
+    Node *newNode = new Node();
+    newNode->data = val;
+    newNode->next = NULL;
+    Node * temp = head;
+    Node * prev = NULL;
+    int p = 1;
+    while(temp->next != NULL && p < pos) {
+        prev = temp;
+        temp = temp->next;
+        p++;
+    }
+    prev->next = newNode;
+    newNode->next = temp;
+}
+
+void LinkedList::display() {
+    for(Node *temp = head; temp != NULL; temp = temp->next)
+        cout<<temp->data<<"->";
+}
+
+int main() {
+    LinkedList lst;
+    lst.addFirst(30);
+    lst.addFirst(20);
+    lst.addFirst(10);
+    cout<<"Add First:"<<endl;
+    lst.display();
+    lst.addLast(40);
+    cout<<"\nAdd Last:"<<endl;
+    lst.display();
+    lst.insertPosition(4, 35);
+    cout<<"\nInsert Position:"<<endl;
+    lst.display();
+    return 0;
+}
+
+/* Output:
+Add First:
+10->20->30->
+Add Last:
+10->20->30->40->
+Insert Position:
+10->20->30->35->40->
+*/
